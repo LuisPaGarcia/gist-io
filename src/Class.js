1f02c1928086f56c1ea460308b7619734a49647b
LuisPa Garcia <diganluispa@gmail.com>
2021-02-11 18:40:53 -0600

    refactor: WIP adding paged

---
+++ src/Class.js
@@ -0,0 +1,63 @@
+const githubBase = require("github-base");
+/**
+ * Instance to get or patch an gist ID
+ */
+class CrudGist {
+  /**
+   *
+   * @param {string} gistId a valid gist ID
+   * @param {string} filename name of the file inside the gist
+   */
+  constructor({ token, username, gistId, filename }) {
+    this.token = token;
+    this.username = username;
+    this.gistId = gistId;
+    this.fileName = filename;
+    this.Github = new githubBase({
+      token: this.token,
+    });
+  }
+
+  async paged() {
+    try {
+      return this.Github.paged(`/users/${this.username}/gists`).then(
+        (res) => res.pages[0].body
+      );
+    } catch (error) {
+      throw new Error("Whoops!");
+    }
+  }
+
+  /**
+   * function to get the value of the gist
+   * @return {Object} content of the gist as an object
+   */
+  async get() {
+    try {
+      return await this.Github.get(`/gists/${this.gistId}`).then((response) =>
+        JSON.parse(response.body.files[this.fileName].content)
+      );
+    } catch (error) {
+      console.log(error);
+      return {};
+    }
+  }
+  /**
+   * function to update the content of the gist
+   * @param {object} newContent new content of the gist
+   * @return {Boolean} true if the update was successful, false if have fail
+   */
+  async patch(newContent) {
+    try {
+      await this.Github.patch(`/gists/${this.gistId}`, {
+        files: { [this.fileName]: { content: JSON.stringify(newContent) } },
+      });
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
+
+module.exports = CrudGist;
